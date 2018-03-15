74e134f0560ebfa7d2a483cb3fcb0563e2f82668
engle89 <37059264+engle89@users.noreply.github.com>
2018-03-16 03:19:21 +0800

    Add files via upload

---
+++ def_add_layer.py
@@ -0,0 +1,57 @@
+from __future__ import print_function
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def add_layer(inputs, in_size, out_size, activation_function=None):
+    weights = tf.Variable(tf.random_normal([in_size, out_size]))
+    biases = tf.Variable(tf.zeros([1, out_size])+0.1)
+    wx_plus_b = tf.matmul(inputs, weights)+biases
+    if activation_function is None:
+        outputs = wx_plus_b
+    else:
+        outputs = activation_function(wx_plus_b)
+    return outputs
+
+
+# data
+x_data = np.linspace(-1, 1, 300)[:, np.newaxis]
+noise = np.random.normal(0, 0.05, x_data.shape)
+y_data = np.square(x_data) - 0.5 + noise
+
+# placeholder
+xs = tf.placeholder(tf.float32, [None, 1])
+ys = tf.placeholder(tf.float32, [None, 1])
+
+# add hidden layer
+l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
+
+# add output layer
+prediction = add_layer(l1, 10, 1, activation_function=None)
+
+# error
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys-prediction), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+# plot
+fig = plt.figure()
+ax = fig.add_subplot(1, 1, 1)
+ax.scatter(x_data, y_data)
+plt.ion()
+plt.show()
+
+for i in range(1000):
+    sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
+    if i % 50 == 0:
+        try:
+            ax.lines.remove(lines[0])
+        except Exception:
+            pass
+        prediction_value = sess.run(prediction, feed_dict={xs: x_data, ys: y_data})
+        lines = ax.plot(x_data, prediction_value, 'r-', lw=5)
+        plt.pause(1)
